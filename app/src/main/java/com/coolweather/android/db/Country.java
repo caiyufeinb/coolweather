c7346f6f23696cb342ef8fba0bf7a997a6f1ae84
Immortal <1013032271@qq.com>
2018-10-20 15:03:10 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class Country extends LitePalSupport {
+    private int id;
+
+    private String countryName;
+
+    private int cityId;
+
+    private String weatherId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
